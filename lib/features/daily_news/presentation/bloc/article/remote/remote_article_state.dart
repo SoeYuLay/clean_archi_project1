a2a8a402af8a93cec9f2a1be60003d5c6a8684eb
Soe Yu Lay <zaya532002@gmail.com>
2026-04-03 14:23:16 +0630

    bloc

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,27 @@
+import 'package:clean_archi_project1/features/daily_news/domain/entities/article.dart';
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class RemoteArticlesState extends Equatable {
+  final List<ArticleEntity> ? articles;
+  final DioException ? error;
+
+  const RemoteArticlesState({this.articles, this.error});
+
+  @override
+  List<Object> get props => [articles!,];
+}
+
+class RemoteArticlesLoading extends RemoteArticlesState {
+  const RemoteArticlesLoading();
+}
+
+class RemoteArticlesDone extends RemoteArticlesState {
+  const RemoteArticlesDone({required List<ArticleEntity> articles})
+      : super(articles: articles);
+}
+
+class RemoteArticlesError extends RemoteArticlesState {
+  const RemoteArticlesError({required DioException error})
+      : super(error: error);
+}
