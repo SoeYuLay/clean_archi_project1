725025f080cbffb9fe0b12dbe2148e59c1aca1ff
Soe Yu Lay <zaya532002@gmail.com>
2026-04-10 15:10:52 +0630

    add unit test for get saved articles, save and delete article for local

--- test/helper/test_data.dart
+++ test/helper/test_data.dart
@@ -1,3 +1,4 @@
+import 'package:clean_archi_project1/features/daily_news/data/data_sources/local/models/articleEntity.dart';
 import 'package:clean_archi_project1/features/daily_news/data/data_sources/remote/models/articleDto.dart';
 import 'package:clean_archi_project1/features/daily_news/domain/entities/article.dart';
 import 'package:clean_archi_project1/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
@@ -14,6 +15,27 @@ class TestData{
     content: 'Full content here...',
     ),
     ArticleDto(
+    title: 'Flutter Clean Architecture1',
+    author: 'Soe Yu Lwin1',
+    description: 'Clean architecture in Flutter1',
+    urlToImage: 'https://example.com/image.jpg',
+    url: 'https://example.com/article',
+    publishedAt: '2026-04-10T10:00:00Z',
+    content: 'Full content here...',
+    )
+  ];
+
+  static final testArticleEntity = [
+    ArticleEntity(
+    title: 'Flutter Clean Architecture',
+    author: 'Soe Yu Lwin',
+    description: 'Clean architecture in Flutter',
+    urlToImage: 'https://example.com/image.jpg',
+    url: 'https://example.com/article',
+    publishedAt: '2026-04-10T10:00:00Z',
+    content: 'Full content here...',
+    ),
+    ArticleEntity(
     title: 'Flutter Clean Architecture1',
     author: 'Soe Yu Lwin1',
     description: 'Clean architecture in Flutter1',
