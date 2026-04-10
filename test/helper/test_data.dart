2d4117c9de096acf5773f1d42096f6ef6fe4d96c
Soe Yu Lay <zaya532002@gmail.com>
2026-04-10 14:32:16 +0630

    add unit test for getting Articles from api service

---
+++ test/helper/test_data.dart
@@ -0,0 +1,49 @@
+import 'package:clean_archi_project1/features/daily_news/data/data_sources/remote/models/articleDto.dart';
+import 'package:clean_archi_project1/features/daily_news/domain/entities/article.dart';
+import 'package:clean_archi_project1/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+class TestData{
+  static final testArticleDto = [
+    ArticleDto(
+    title: 'Flutter Clean Architecture',
+    author: 'Soe Yu Lwin',
+    description: 'Clean architecture in Flutter',
+    urlToImage: 'https://example.com/image.jpg',
+    url: 'https://example.com/article',
+    publishedAt: '2026-04-10T10:00:00Z',
+    content: 'Full content here...',
+    ),
+    ArticleDto(
+    title: 'Flutter Clean Architecture1',
+    author: 'Soe Yu Lwin1',
+    description: 'Clean architecture in Flutter1',
+    urlToImage: 'https://example.com/image.jpg',
+    url: 'https://example.com/article',
+    publishedAt: '2026-04-10T10:00:00Z',
+    content: 'Full content here...',
+    )
+  ];
+
+  static final testArticle = [
+    Article(
+    title: 'Flutter Clean Architecture',
+    author: 'Soe Yu Lwin',
+    description: 'Clean architecture in Flutter',
+    urlToImage: 'https://example.com/image.jpg',
+    url: 'https://example.com/article',
+    publishedAt: '2026-04-10T10:00:00Z',
+    content: 'Full content here...',
+    ),
+    Article(
+    title: 'Flutter Clean Architecture1',
+    author: 'Soe Yu Lwin1',
+    description: 'Clean architecture in Flutter1',
+    urlToImage: 'https://example.com/image.jpg',
+    url: 'https://example.com/article',
+    publishedAt: '2026-04-10T10:00:00Z',
+    content: 'Full content here...',
+    )
+  ];
+}
+
+final remoteArticleError = RemoteArticleError('Failed to fetch articles');
