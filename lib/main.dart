6dd25b594f2e4ffb13596a48427558e5d571233c
Soe Yu Lay <zaya532002@gmail.com>
2026-04-24 08:55:41 +0630

    add firebase initialization in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:clean_archi_project1/config/app_router.dart';
 import 'package:clean_archi_project1/config/app_themes.dart';
 import 'package:clean_archi_project1/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:clean_archi_project1/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:clean_archi_project1/firebase_options.dart';
 import 'package:clean_archi_project1/injection_container.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
