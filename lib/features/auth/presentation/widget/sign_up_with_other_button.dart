188c9dc8b6335b28740df1102b210ad781479c07
SoeYuLay <zaya532002@gmail.com>
2026-05-20 14:57:55 +0630

    add Svg picture import

--- lib/features/auth/presentation/widget/sign_up_with_other_button.dart
+++ lib/features/auth/presentation/widget/sign_up_with_other_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignUpWithOtherButton extends StatelessWidget {
   final String buttonText;
